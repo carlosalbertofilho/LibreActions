ec3ec785ddf4984a6bc6637267fd692640fe3adc
carlosalbertofilho <carlosalberto_filho@outlook.com>
2022-06-24 16:54:08 -0300

    Conexão com o Postgres no docker, sequelize configurado e adicionado Jest

--- JS/TDD_Jest/src/config/database.js
+++ JS/TDD_Jest/src/config/database.js
@@ -1,23 +1,14 @@
-{
-  "development": {
-    "username": "root",
-    "password": null,
-    "database": "database_development",
-    "host": "127.0.0.1",
-    "dialect": "mysql"
+module.exports = {
+  host: '127.0.0.1',
+  port: '15432',
+  username: 'docker',
+  password: 'docker',
+  database: 'docker',
+  dialect: 'postgres',
+  operatorsAliases: false,
+  logging: false,
+  define: {
+    timestamps: true,
+    underscored: true,
   },
-  "test": {
-    "username": "root",
-    "password": null,
-    "database": "database_test",
-    "host": "127.0.0.1",
-    "dialect": "mysql"
-  },
-  "production": {
-    "username": "root",
-    "password": null,
-    "database": "database_production",
-    "host": "127.0.0.1",
-    "dialect": "mysql"
-  }
-}
+};
